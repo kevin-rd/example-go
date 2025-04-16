7e07286812e20bc6ed26d4531566e854c41c936c
kevin@zkme.xyz <kevin@zkme.xyz>
2025-04-16 09:59:05 +0800

    refactor(internal/server): improve logging and request handling

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -11,24 +11,29 @@ import (
 // HandleHello 处理 /hello 请求
 func HandleHello(w http.ResponseWriter, r *http.Request, log *zap.Logger) {
 	start := time.Now()
+	traceId := uuid.New().String()
 
 	// 记录请求日志
 	log.Info("receive request: hello",
+		zap.String("trace_id", traceId),
+		zap.String("log_tag", "module01"),
 		zap.String("method", r.Method),
 		zap.String("url", r.URL.String()),
 		zap.String("client_ip", r.RemoteAddr),
 		zap.Int8("Int8", 28),
-		zap.String("trace_id", uuid.New().String()),
-		zap.String("log_tag", "module1"),
 	)
 
 	// 处理请求
+	value := r.URL.Query().Get("key")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello, Zap!\n"))
 
 	// 记录处理时间
 	duration := time.Since(start)
 	log.Info("handle complete",
+		zap.String("trace_id", traceId),
+		zap.String("log_tag", "module1"),
 		zap.Duration("duration", duration),
+		zap.String("key_test", value),
 	)
 }
