c8e8bb352351965e6e1d40169d9f72eabd631a8c
kevin@zkme.xyz <kevin@zkme.xyz>
2025-04-21 11:24:58 +0800

    feat(logger): enhance zap logger with environment awareness
    - Configure logger to output JSON in Kubernetes environments
    - Use console encoder for local development
    - Add custom time format and colorized output

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,15 +1,36 @@
 package logger
 
 import (
-	"fmt"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+	"time"
 )
+
+const TimeFormatDataTimeMill = "2006-01-02 15:04:05.000-07"
 
 // InitLogger 初始化 zap 日志
 func InitLogger() *zap.Logger {
-	logger, err := zap.NewProduction()
-	if err != nil {
-		panic(fmt.Sprintf("fail to init zap logger: %v", err))
+	config := zap.NewProductionEncoderConfig()
+	config.EncodeTime = func(t time.Time, encoder zapcore.PrimitiveArrayEncoder) {
+		encoder.AppendString(t.Format(TimeFormatDataTimeMill))
 	}
-	return logger
+	config.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	config.EncodeCaller = zapcore.FullCallerEncoder
+
+	var encoder zapcore.Encoder
+	var core zapcore.Core
+	if isRunningInKubernetes() {
+		encoder = zapcore.NewJSONEncoder(config)
+		core = zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), zapcore.DebugLevel)
+	} else {
+		encoder = zapcore.NewConsoleEncoder(config)
+		core = zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), zapcore.InfoLevel)
+	}
+	return zap.New(core)
+}
+
+func isRunningInKubernetes() bool {
+	_, exists := os.LookupEnv("KUBERNETES_SERVICE_HOST")
+	return exists
 }
